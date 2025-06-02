7f4a1700da18c4a175367b0ee51369e3a6db92c2
KaweeshaAvindu <kaweeshaavindu@gmail.com>
2025-06-02 20:34:42 +0530

    Add website files including HTML, CSS, and images
    Uploaded the initial version of the dairy product website with all HTML, CSS, and image assets.

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("nav01").innerHTML =
+
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " TR Production. All rights reserved.</p>";
+
+
